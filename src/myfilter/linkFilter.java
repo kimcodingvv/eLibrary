bdd50283fcc0197f385731fba82061b8a4717a20
kimcodingvv <junghun1997@naver.com>
2020-06-11 17:01:13 +0900

    Link Filter

---
+++ src/myfilter/linkFilter.java
@@ -0,0 +1,51 @@
+package myfilter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class linkFilter
+ */
+@WebFilter("/linkFilter")
+
+public class linkFilter implements Filter {
+
+	public linkFilter() {
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		 HttpServletRequest httprequest = (HttpServletRequest) request;
+		 String url[] = httprequest.getRequestURI().toString().trim().split("/");
+		String action = url[url.length - 1];
+		if (excludeUrl(action)) {
+			chain.doFilter(httprequest, response);
+		} else {
+			RequestDispatcher rd = httprequest.getRequestDispatcher("route?action=" + action);
+           rd.forward(httprequest, response);
+		}
+	}
+
+	private boolean excludeUrl(String action) {
+		if (action.equals("catalog")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
